2ab6f40eee957ffea9a337402343223c75bbc0ff
Adnan Abdulhussein <adnan@prydoni.us>
2017-11-17 13:49:02 +0000

    add login/logout to mobile menu (#22)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Angulartics2GoogleAnalytics } from 'angulartics2';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MenuService } from './shared/services/menu.service';
 import { ChartsService } from './shared/services/charts.service';
 import { ConfigService } from './shared/services/config.service';
 import { SeoService } from './shared/services/seo.service';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,20 @@ import { SeoService } from './shared/services/seo.service';
   styleUrls: ['./app.component.scss'],
   providers: [MenuService, ChartsService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Show the global menu
   public showMenu: boolean = false;
   // Config
   public config;
+  public loggedIn: boolean = false;
 
   constructor(
     angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics,
     config: ConfigService,
     private menuService: MenuService,
     private router: Router,
-    private seo: SeoService
+    private seo: SeoService,
+    private authService: AuthService,
   ) {
     menuService.menuOpen$.subscribe(show => {
       this.showMenu = show;
@@ -34,5 +37,15 @@ export class AppComponent {
       menuService.hideMenu();
     });
     this.config = config;
+  }
+
+  ngOnInit() {
+    this.authService.loggedIn().subscribe(loggedIn => { this.loggedIn = loggedIn; });
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => {
+      window.location.reload();
+    });
   }
 }
